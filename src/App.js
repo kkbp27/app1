1c869fce6c1b1ac43e2b3371f65f7ebcf9c71966
kktp27 <kotchakon39@gmail.com>
2023-12-25 11:08:37 +0700

    Update App.js insert variable
    Update App.js insert variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function App() {
-  let r='React'
+  let r='<b>React<b/>'
   let rn='React Native'
   const el=(
     <div>
       Create Web & Mobile Apps<br/>
-      whit <span dangerouslySetInnerHTML={{_html: r}}/> & {rn}
+      whit <span dangerouslySetInnerHTML={{__html: r}}/> & {rn}
     </div>
   )
   return el
