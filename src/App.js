ee79c0dc99317025675924441d5f4cdf200873a8
kktp27 <kotchakon39@gmail.com>
2024-01-08 11:37:39 +0700

    Add Class-components.js And Update App.js
    Add Class-components.js And Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Content, Footer, Header } from './func-components';
+import Calender from './Class-components';
 //import './style.css'
 
 function App() {
   return(
-    [<Header/>,<Content/>,<Footer/>]
+    // [<Header/>,<Content/>,<Footer/>]
+    <>
+    <Header/>
+    <p><center><Calender/></center></p>
+    <Content/>
+    <Footer/>
+    </>
   )
 }
 
