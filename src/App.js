40d8901642c5269756bc8400770b9b54f76f0155
kktp27 <kotchakon39@gmail.com>
2024-01-15 11:33:29 +0700

    EventData n function component
    EventData n function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Button from './Class-components'
 import { Calculator2 } from './calculator';
-
+import { EventData2 } from './event-data';
 function App() {
-    return <Calculator2/>
+    return <EventData2/>
 }
-
 export default App;
